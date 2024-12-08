78a2e23c6f7bfa6e9bdf296bc2469c607cc0f9ce
dimit <dimitar_rradev@abv.bg>
2024-12-08 12:02:54 +0200

    first commit

---
+++ src/main/java/bg/softuni/javaautomappingobjectslab/JavaAutoMappingObjectsLabApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.javaautomappingobjectslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAutoMappingObjectsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAutoMappingObjectsLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/javaautomappingobjectslab/JavaAutoMappingObjectsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.javaautomappingobjectslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAutoMappingObjectsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
